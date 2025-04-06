274caf91e50251ef01e845f073a02c4205bf2214
alenachankunju <alenachankunju@gmail.com>
2025-04-06 19:58:14 +0530

    min expeses updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
 
   const [totalExpenses, setTotalExpenses] = useState(() => {
     const savedTotalExpenses = localStorage.getItem("expenses");
-    return savedTotalExpenses ? parseFloat(savedTotalExpenses) : 5000;
+    return savedTotalExpenses ? parseFloat(savedTotalExpenses) : 50000;
   });
   const [transactions, setTransactions] = useState(() => {
     const savedTransactions = localStorage.getItem("");
